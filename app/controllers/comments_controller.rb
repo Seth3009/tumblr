fc79bc04bf9407b6734231599fa3868d42d167c0
Setiadi <setiadi.seth@gmail.com>
2023-06-09 11:28:28 +0700

    delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :get_post, only: [:create, :destroy]
+  
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to @post
   end
 
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
   private
+  def get_post
+    @post = Post.find(params[:post_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:name, :body, :post_id)
   end
